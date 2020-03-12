e8d64a1ce7b1f6be20b26fae40eea3c3657a7c0b
Raphael Koszalka <rmkoszalka@gmail.com>
2020-03-12 15:53:57 -0300

    Pages

--- src/app/app.route.module.ts
+++ src/app/app.route.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
+import {AboutComponent} from './pages/about/about.component';
+import {PortfolioComponent} from './pages/portfolio/portfolio.component';
+import {ContactComponent} from './pages/contact/contact.component';
 
 
 const routes: Routes = [{
   path: '',
   component: HomeComponent,
+},{
+  path: 'about-me',
+  component: AboutComponent,
+},{
+  path: 'portfolio',
+  component: PortfolioComponent,
+},{
+  path: 'contact',
+  component: ContactComponent,
 }];
 
 @NgModule({
